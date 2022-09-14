9f41866b03e3d7a32eb3fcac492ed62dde6bb28d
sefa emre baskaya <73946291+seb-07@users.noreply.github.com>
2022-09-14 13:46:36 +0300

    Create main.py

---
+++ GPS/main.py
@@ -0,0 +1,47 @@
+import serial
+from time import sleep
+import sys
+
+ser = serial.Serial ("/dev/ttyAMA0")
+gpgga_info = "$GPGGA,"
+GPGGA_buffer = 0
+NMEA_buff = 0
+
+def convert_to_degrees(raw_value):
+    decimal_value = raw_value/100.00
+    degrees = int(decimal_value)
+    mm_mmmm = (decimal_value - int(decimal_value))/0.6
+    position = degrees + mm_mmmm
+    position = "%.4f" %(position)
+    return position
+
+try:
+    while True:
+        received_data = (str)(ser.readline()) #read NMEA string received
+        GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string
+        if (GPGGA_data_available>0):
+            GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after “$GPGGA,” string
+            NMEA_buff = (GPGGA_buffer.split(","))
+            nmea_time = []
+            nmea_latitude = []
+            nmea_longitude = []
+            nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
+            nmea_latitude =NMEA_buff[1]                #extract latitude from GPGGA string
+            nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
+
+            if(type(nmea_latitude)==str):
+                nmea_latitude=0
+                nmea_longitude=0
+
+            lat = (float)(nmea_latitude)
+            lat = convert_to_degrees(lat)
+            longi = (float)(nmea_longitude)
+            longi = convert_to_degrees(longi)
+            print ("NMEA Latitude:", lat,"NMEA Longitude:", longi,"\n")
+except KeyboardInterrupt:
+    sys.exit(0)
+
+
+
+
+
